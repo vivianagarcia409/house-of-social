1f71dd7eeaa1b8844ed4eb0cae7f2e4565689b0a
Viviana Garcia <vivianandrea4@gmail.com>
2021-08-05 17:15:09 -0400

    semi fixed about intro

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -14,21 +14,21 @@ function AboutUs(props) {
             </div>
             <div className="about-them">
             House of Social is a full service event company offering production 
-            and management services for a broad range of clients which include social, 
+            and management services which include social, 
             corporate, and non-profit events.
             <br />
             <br />
             Women-owned and operated, Samantha Santos and Daniella Gonzalez, House of 
             Social was conceived in 2020 when two industry professionals came together 
-            to create unique experiences. Sam and Daniella met when they worked together 
-            at a marketing agency in South Florida, creating activations and producing 
-            galas for clients that serviced a range of 800 to thousands of attendees.
+            to create unique experiences. 
             <br />
             <br />
-            Branching out on their own, House of Social has serviced social and corporate 
-            clients. Their unique strategy is to personalize each experience, cater to individual 
-            requests, and inspire other attendees with their ingenuity and creativity. 
+            Sam and Daniella met working together at a marketing
+            agency in South Florida. Branching out on their own during the pandemic with House
+            of Social, their unique strategy to personalize each experience and inspire other attendees
+            with their ingenuity and creativity makes them stand out from the rest.
 
+           
             </div>
 
             <div className="individuals">
