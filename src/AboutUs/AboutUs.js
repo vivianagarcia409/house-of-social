8df00b9f44ab0f1dd252366cc3c018468da2f35e
Viviana Garcia <vivianandrea4@gmail.com>
2021-08-06 10:29:00 -0400

    semi finished about page

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './AboutUs.css';
 import lelasam from '../images/lela-sam.jpeg'
-import lela from '../images/lela.jpeg'
+import sam from '../images/sam.png'
+import lela from '../images/lela2.png'
 
 function AboutUs(props) {
     return (
@@ -19,7 +20,7 @@ function AboutUs(props) {
             <br />
             <br />
             Women-owned and operated, Samantha Santos and Daniella Gonzalez, House of 
-            Social was conceived in 2020 when two industry professionals came together 
+            Social was created in 2020 when two industry professionals came together 
             to create unique experiences. 
             <br />
             <br />
@@ -32,8 +33,42 @@ function AboutUs(props) {
             </div>
 
             <div className="individuals">
-                <img src={lela} className="lela" alt="lela" />
 
+                <div className="sam-container">
+                    <h1 className="sam-title">
+                        - Samantha Santos -
+                    </h1>
+                    
+                     <img src={sam} className="sam" alt="sam" />
+
+                    <div className="sam-statement">
+                    “When the pandemic put the events industry on hold, 
+                    I needed an outlet for my love of planning. It’s been 
+                    a passion of mine for as long as I can remember and 
+                    house of social became the space where I could pour 
+                    my heart into what I love the most.”
+                    </div>
+                </div>  
+           
+                <div className="lela-container">
+                        <h1 className="lela-title">
+                            - Daniella Gonzalez -
+                        </h1>
+                        <img src={lela} className="lela" alt="lela" />
+                    <div className="lela-statement">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing 
+                    elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam, quis 
+                    nostrud exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore 
+                    eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia 
+                    deserunt mollit anim id est laborum."
+                    </div>
+                </div> 
+        
+            
             </div>
 
 
